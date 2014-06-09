e4cb64fbcb272c91de6d27ee2659eb243a0de103
Jean-Charles Sisk <jasisk@gmail.com>
2014-06-09 08:12:22 -0400

    adding better init loading

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // var stream = require('./test/fixtures/eventsource')();
 var temperature = require('./lib/temperature')();
-var filter = require('./lib/filter')();
+var filterGen = require('./lib/filter');
 var stream = require('./lib/stream');
 var database = require('./lib/db');
 var pipe = require('./lib/pipe');
@@ -15,17 +15,22 @@ var port = process.env.PORT || 8000;
 var server;
 
 database.getLatest(function (err, initial) {
-  temperature.set({data: initial.temperature, published_at: initial.ts});
-});
+  var setTemp, filter;
 
-pipe([stream, filter, db, temperature.stream]);
-filter.on('suppressed', temperature.handler.bind(temperature));
+  temperature.set({data: initial.temperature, published_at: initial.ts});
+  initial.temperature !== '??.??' && (setTemp = initial.temperature);
+
+  filter = filterGen(setTemp);
+  pipe([stream, filter, db, temperature.stream]);
+  filter.on('suppressed', temperature.handler.bind(temperature));
+
+  server.listen(port, function () {
+    console.log('Listening on %j', server.address());
+  });
+});
 
 server = http.createServer();
 server.on('request', requestHandler);
-server.listen(port, function () {
-  console.log('Listening on %j', server.address());
-});
 
 function requestHandler(req, res) {
   var sendCode = respondWithCode.bind(this, req, res);
